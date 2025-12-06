347891e20a2014c76483aa2d5c08506e890cc016
Matthew Leach <dev@mattleach.net>
2025-12-06 21:37:38 +0000

    arch: arm64: parse cmdline from chosen node
    The kernel command line is passed via the FDT's chosen node. Use that,
    rather than hard-coding it into the kernel.
    
    Fixes: #20

--- src/arch/arm64/fdt.rs
+++ src/arch/arm64/fdt.rs
@@ -1,1 +1,10 @@
+use crate::drivers::fdt_prober::get_fdt;
+use alloc::string::{String, ToString};
+
 pub const MAX_FDT_SZ: usize = 2 * 1024 * 1024;
+
+pub fn get_cmdline() -> Option<String> {
+    let fdt = get_fdt();
+
+    Some(fdt.chosen()?.bootargs()?.to_string())
+}
